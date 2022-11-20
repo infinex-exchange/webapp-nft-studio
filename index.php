f80adc4a1efa5e8e6eeceebc8be55087bdec0df5
vayamos-exchange <admin@vayamos.cc>
2022-11-20 17:35:54 +0100

    Layout from bridge

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     <head>
         <?php include('../../../templates/head.php'); ?>
         <link rel="stylesheet" href="/nft/css/styles.css?<?php echo filemtime(__DIR__.'/../css/styles.css'); ?>">
-        <link rel="stylesheet" href="/nft/studio/css/styles.css?<?php echo filemtime(__DIR__.'/css/styles.css'); ?>">
         <title>Vayamos NFT Studio</title>
     </head>
     <body class="body-background">
@@ -18,57 +17,203 @@
         <!-- Root container -->
         <div id="root" class="container-fluid container-1500 h-rest pt-2 p-0">
         
-            <div class="studio-jumbotron row m-0 px-4 py-3 py-lg-5">
-                <div class="col-12">
-                    <h1>Vayamos NFT</h1>
-                    <p>Discover exclusive NFT collections from the world's top artists.</p>
+            <div class="row h-rest m-0">
+                <div class="col-12 col-lg-6 me-auto my-auto px-4 py-5">
+                    <div class="row">
+                        <div class="col-12 jumbotron">
+                            <h1>Vayamos Bridge</h1>
+                            <p>
+                                With Vayamos Bridge you can wrap any asset supported by Vayamos Exchange to BPX blockchain tokens.
+                                You can hold your wrapped tokens in your BPX wallet, trade them using offers and transfer them without fee.
+                                BPX tokens are pegged on a 1:1 basis to the underlying asset.
+                                You can redeem your wrapped tokens back to the original asset at any time.
+                                This cross-chain bridge improves interoperability between BPX and other blockchains
+                                and gives you direct access to the world of DeFi on the BPX blockchain.
+                            </p>
+                        </div>
+                        <div class="col-9 mx-auto d-none d-lg-block">
+                            <img src="/bridge/img/bridge.png" class="img-fluid">
+                        </div>
+                    </div>
                 </div>
-            </div>
             
-            <div class="row gx-0 gx-lg-3 gy-3 m-0">
-                
-                <div class="col-12">
-        	        <div class="p-2 p-lg-4 background rounded">
-        	            <div class="row">
-        	                <div class="col-12 pt-2 pb-4 text-center">
-                                <h3>Ending soon</h3>
-        		            </div>
-        		            <div class="col-12">
-                                <div id="last-minute-data" class="row">
+                <div class="col-12 col-lg-5 p-0 my-auto">
+                    <div class="p-2 p-lg-4 ui-card-light rounded">
+                        <div id="bridge-step1">
+                            <div class="row pt-1 pb-3">
+                                <div class="col-6 ps-2 pe-1">
+                                    <button type="button" class="btn btn-primary w-100 peg-toggle" data-side="peg-in">Peg-in</button>
                                 </div>
-        		            </div>
-        	            </div>
-        	        </div>
-        	    </div>
-                
-                <div class="col-12">
-        	        <div class="p-2 p-lg-4 background rounded">
-        	            <div class="row">
-        	                <div class="col-12 pt-2 pb-4 text-center">
-                                <h3>Popular NFTs</h3>
-        		            </div>
-        		            <div class="col-12">
-                                <div id="featured-nft-data" class="row">
+                                <div class="col-6 ps-1 pe-2">
+                                    <button type="button" class="btn btn-secondary w-100 peg-toggle" data-side="peg-out">Peg-out</button>
                                 </div>
-        		            </div>
-        	            </div>
-        	        </div>
-        	    </div>
-                
-                <div class="col-12">
-        	        <div class="p-2 p-lg-4 background rounded">
-        	            <div class="row">
-        	                <div class="col-12 pt-2 pb-4 text-center">
-                                <h3>Low price</h3>
-        		            </div>
-        		            <div class="col-12">
-                                <div id="low-price-data" class="row">
+                            </div>
+                            <div class="row pt-3">
+                                <h3 class="peg-in-text">Asset to peg-in:</h3>
+                                <h3 class="peg-out-text">Asset to peg-out:</h3>
+                            </div>
+                            <div class="row pb-3">
+                                <?php include(__DIR__.'/../../templates/select_coin.php'); ?>
+                            </div>
+                            <div class="row pt-3">
+                                <h3 class="peg-in-text">Source network:</h3>
+                                <h3 class="peg-out-text">Destination network:</h3>
+                            </div>
+                            <div class="row pb-3">
+                                <?php include(__DIR__.'/../../templates/select_net.php'); ?>
+                            </div>
+                            <div class="row pt-3">
+                                <h3>Destination address:</h3>
+                            </div>
+                            <div class="row pb-3">
+                                <form>
+                                <input id="peg-target-addr" type="text" placeholder="Paste address" class="form-control" autocomplete="off" readonly>
+                                </form>
+                                <small id="help-target-addr" class="form-text" style="display: none">Address is invalid</small>
+                            </div>
+                            <div class="row pt-3 peg-memo-wrapper">
+                                <h3 id="peg-memo-name"></h3>
+                            </div>
+                            <div class="row pb-3 peg-memo-wrapper">
+                                <form>
+                                <input type="text" class="form-control" id="peg-target-memo" placeholder="Optional">
+                                </form>
+                                <small id="help-target-memo" class="form-text" style="display: none">Invalid format</small>
+                            </div>
+                            <div class="row pb-3 flex-nowrap">
+                                <div class="col-auto me-auto">
+                                    <h5 class="secondary">Fee:</h5>
                                 </div>
-        		            </div>
-        	            </div>
-        	        </div>
-        	    </div>
+                                <div class="col-auto">
+                                    <span class="peg-fee-val">-</span>
+                                    <span class="peg-fee-assetid"></span>
+                                </div>
+                            </div>
+                            <div class="row pt-3 pb-1">
+                                <form id="peg-step1-form">
+                                    <button type="submit" class="btn btn-primary d-block ms-auto w-25">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                        
+                        <div id="bridge-preloader">
+                            <div class="row py-2 text-center h-100">
+                                <i class="fa-solid fa-spinner fa-spin-pulse fa-2x my-auto"></i>
+                            </div>
+                        </div>
+                        
+                        <div id="bridge-step2">
+                            <div class="row pt-1 pb-3">
+                                <h3>Your bridge has been created!</h3>
+                            </div>
+                            <div class="row py-3 flex-nowrap justify-content-evenly">
+                                <div class="col-auto text-center">
+                                    <div class="p-2 rounded text-center" style="background-color: var(--color-input); min-width: 100px;">
+                                        <img id="peg-from-net-img" class="p-2" width="40" height="40" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
+                                        <br>
+                                        <span id="peg-from-net" class="p-2 secondary"></span>
+                                    </div>
+                                </div>
+                                <div class="col-auto my-auto">
+                                    <i class="fa-solid fa-arrow-right fa-2x"></i>
+                                </div>
+                                <div class="col-auto text-center">
+                                    <div class="p-2 rounded text-center" style="background-color: var(--color-input); min-width: 100px;">
+                                        <img id="peg-to-net-img" class="p-2" width="40" height="40" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
+                                        <br>
+                                        <span id="peg-to-net" class="p-2 secondary"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row pt-3">
+                                <h3>Deposit funds to this address:</h3>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 px-4">
+                                    <div class="row py-2 flex-nowrap rounded" style="background-color: var(--color-input);">
+                                        <div class="col-auto my-auto wrap">
+                                            <span class="wrap" id="peg-deposit-addr"></span>
+                                        </div>
+                                        <div class="col-auto my-auto">
+                                            <a href="#_" class="secondary" data-copy="#peg-deposit-addr" onClick="copyButton(this); event.stopPropagation();"><i class="fa-solid fa-copy fa-xl"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="peg-deposit-memo-wrapper">
+                                <div class="row pt-3">
+                                    <h3 id="peg-deposit-memo-name"></h3>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12 px-4">
+                                    <div class="row py-2 flex-nowrap rounded" style="background-color: var(--color-input);">
+                                        <div class="col-auto my-auto wrap">
+                                            <span class="wrap" id="peg-deposit-memo"></span>
+                                        </div>
+                                        <div class="col-auto my-auto">
+                                            <a href="#_" class="secondary" data-copy="#peg-deposit-memo" onClick="copyButton(this); event.stopPropagation();"><i class="fa-solid fa-copy fa-xl"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="row pt-3 flex-nowrap">
+                                <div class="col-auto me-auto">
+                                    <h5 class="secondary">Confirmations required:</h5>
+                                </div>
+                                <div id="peg-conf-target" class="col-auto">
+                                    -
+                                </div>
+                            </div>
+                            <div class="row pb-3 flex-nowrap">
+                                <div class="col-auto me-auto">
+                                    <h5 class="secondary">Fee:</h5>
+                                </div>
+                                <div class="col-auto">
+                                    <span class="peg-fee-val">-</span>
+                                    <span class="peg-fee-assetid"></span>
+                                </div>
+                            </div>
+                            
+                            <div class="row py-3">
+                                <div class="col-auto my-auto text-center" style="width: 60px">
+                                    <i style="color: var(--color-ultra)" class="fa-solid fa-infinity fa-2x"></i>
+                                </div>
+                                <div class="col small my-auto">
+                                    Once created bridge is valid forever and can be used unlimited number of times.
+                                    <br>
+                                    You don't have to worry about the address expiry date.
+                                </div>
+                            </div>
+                            <div class="row py-3">
+                                <div class="col-auto my-auto text-center" style="width: 60px">
+                                    <i style="color: var(--color-ultra)" class="fa-solid fa-money-check-dollar fa-2x"></i>
+                                </div>
+                                <div class="col small my-auto">
+                                    Deposit any amount to transfer tokens between networks.
+                                    <br>
+                                    The fee shown above will be deducted from the deposited amount.
+                                </div>
+                            </div>
+                            <div class="row pt-3 pb-1">
+                                <div class="col-auto my-auto text-center" style="width: 60px">
+                                    <i style="color: var(--color-ultra)" class="fa-solid fa-triangle-exclamation fa-2x"></i>
+                                </div>
+                                <div class="col small my-auto">
+                                    If the deposit amount is not enough to pay the fee, the funds will be waiting for
+                                    <br>
+                                    the next transaction to this bridge
+                                </div>
+                            </div>
+                            
+                        </div>                  
+                    </div>
+                </div>
                 
+                <div class="col-7 mx-auto py-5 d-lg-none">
+                    <img src="/bridge/img/bridge.png" class="img-fluid">
+                </div>
+
             </div>
         
         <!-- / Root container -->
