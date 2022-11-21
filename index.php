6a3eb674a7d1e833a7ab2cfe7766fb7f4b8b5a91
vayamos-exchange <admin@vayamos.cc>
2022-11-21 16:40:29 +0100

    Layout

--- index.php
+++ index.php
@@ -124,28 +124,7 @@
                     </span>
                 </div>
                 <div class="col-12 col-md-6 col-lg-4">
-                    <h3>
-                        <i class="fa-solid fa-temperature-three-quarters"></i>
-                        No heat and no noise
-                    </h3>
-                    <span class="secondary">
-                        If you don't have the location to host mining hardware yourself, cloud mining is a perfect choice!
-                    </span>
-                </div>
-            </div>
-            
-            <div class="row m-0 pb-5">
-	            <div class="col-12">
-                    <div class="alert alert-danger d-flex align-items-center my-2" role="alert">
-                        <div class="px-2">
-                            <i class="fa-solid fa-chart-line fa-2x"></i>
-                        </div>
-                        <div class="px-2">
-                            All <span class="billing-asset"></span> estimations are based on the average exchange rate for the last 14 days.<br>
-                            All forecasts are based on the current mining profitability of the offered coins.<br>
-                            Mining profitability varies all the time. Do your own research before purchasing the service.
-                        </div>
-                    </div>
+                    <img src="/nft/studio/img/painting.png" class="img-fluid">
                 </div>
             </div>
         
