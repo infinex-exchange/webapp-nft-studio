97f495dc0be6d0a48ba6366ba680009e349fee5d
infinex-exchange <admin@vayamos.cc>
2023-02-03 15:01:00 +0100

    Add ajax

--- js/collection.js
+++ js/collection.js
@@ -11,5 +11,26 @@ $(document).on('authChecked', function() {
     
     initSelectNet(null, '/nft/wallet/networks', false);
     
-    $(document).trigger('renderingStage');
+    $.ajax({
+        url: config.apiUrl + '/nft/studio/collections',
+        type: 'POST',
+        data: JSON.stringify({
+            api_key: window.apiKey,
+            scolid: 0
+        }),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(data.success) {
+            $(document).trigger('renderingStage');
+        }
+        else {
+            msgBoxRedirect(data.error);
+        }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        msgBoxNoConn(true);
+    });
 });
