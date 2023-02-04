16dcfca120a7fd186e51b1f04ea0dd6caea7ac3b
infinex-exchange <admin@vayamos.cc>
2023-02-05 00:16:30 +0100

    Render projects test

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,64 @@
 $(document).ready(function() {
 	window.renderingStagesTarget = 1;
-    $(document).trigger('renderingStage');
 });
+
+$(document).on('authChecked', function() {
+	if(!window.loggedIn) return;
+	
+	window.projects = new AjaxScroll(
+        $('#projects-data'),
+        $('#projects-data-preloader'),
+        {
+	        api_key: window.apiKey
+        },
+        function() {
+            this.data.offset = this.offset;
+            var thisAS = this;
+            
+            $.ajax({
+                url: config.apiUrl + '/nft/studio/projects',
+                type: 'POST',
+                data: JSON.stringify(thisAS.data),
+                contentType: "application/json",
+                dataType: "json",
+            })
+            .retry(config.retry)
+            .done(function (data) {
+                if(data.success) {
+                    $.each(data.projects, function(k, v) {
+                        thisAS.append(renderProject(v));
+                    });
+                    
+                    thisAS.done();
+            
+                    if(thisAS.offset == 0)
+                        $(document).trigger('renderingStage');
+                        
+                    if(data.projects.length != 50)
+                        thisAS.noMoreData();
+                } else {
+                    msgBoxRedirect(data.error);
+                    thisAS.done();
+                    thisAS.noMoreData();
+                }
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                msgBoxNoConn(true);
+                thisAS.done();
+                thisAS.noMoreData();
+            });
+        },
+        true,
+        true
+    );
+});
+
+function renderProject(data) {
+    return `
+        <div class="row">
+            <div class="col-12">
+                ${data.collection.name}
+            </div>
+        </div>
+    `;
+}
