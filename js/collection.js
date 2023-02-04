015250215a6589db786e0014f6d848825918ad16
infinex-exchange <admin@vayamos.cc>
2023-02-04 11:24:03 +0100

    Get collection for update

--- js/collection.js
+++ js/collection.js
@@ -11,27 +11,40 @@ $(document).on('authChecked', function() {
     
     initSelectNet(null, '/nft/wallet/networks', false);
     
-    $(document).trigger('renderingStage');
-    $.ajax({
-        url: config.apiUrl + '/nft/studio/collections',
-        type: 'POST',
-        data: JSON.stringify({
-            api_key: window.apiKey,
-            scolid: 0
-        }),
-        contentType: "application/json",
-        dataType: "json",
-    })
-    .retry(config.retry)
-    .done(function (data) {
-        if(data.success) {
-            $(document).trigger('renderingStage');
-        }
-        else {
-            msgBoxRedirect(data.error);
-        }
-    })
-    .fail(function (jqXHR, textStatus, errorThrown) {
-        msgBoxNoConn(true);
-    });
+    var pathArray = window.location.pathname.split('/');
+    var scolid = pathArray[pathArray.length - 1];
+    
+    if(scolid == 'add') {
+        $('.title-create').removeClass('d-none');
+        document.title = 'Create collection | ' + document.title;
+        $(document).trigger('renderingStage');
+    }
+    
+    else {
+        $('.title-edit').removeClass('d-none');
+        document.title = 'Edit collection | ' + document.title;
+        
+        $.ajax({
+            url: config.apiUrl + '/nft/studio/collections/get',
+            type: 'POST',
+            data: JSON.stringify({
+                api_key: window.apiKey,
+                scolid: scolid
+            }),
+            contentType: "application/json",
+            dataType: "json",
+        })
+        .retry(config.retry)
+        .done(function (data) {
+            if(data.success) {
+                $(document).trigger('renderingStage');
+            }
+            else {
+                msgBoxRedirect(data.error);
+            }
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            msgBoxNoConn(true);
+        });
+    }
 });
