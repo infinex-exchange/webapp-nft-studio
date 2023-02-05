982360cee324b019873a458b940a4054bdab77af
infinex-exchange <admin@vayamos.cc>
2023-02-05 17:55:12 +0100

    Preview

--- js/render_projects.js
+++ js/render_projects.js
@@ -25,9 +25,12 @@ function renderCollection(data) {
 		`;
     }
     
+    var preview = '/nft/img/no_preview.png';
+    
     return `
       <div class="row hoverable separate px-1 py-2">
           <div class="col-2">
+	          <img src="${preview}" class="img-fluid">
           </div>
           <div class="col-10">
               <div class="row">
