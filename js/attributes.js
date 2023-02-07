0f43632ddc579af73faae864787cee5d16439d62
infinex-exchange <admin@vayamos.cc>
2023-02-07 15:52:56 +0100

    Layout

--- js/attributes.js
+++ js/attributes.js
@@ -5,13 +5,13 @@ function removeAttribute(elem) {
 function addAttribute(key = '', value = '') {
     $('#attributes').append(`
         <div class="row attribute-item">
-            <div class="col-4">
+            <div class="col-5">
                 <input type="text" class="form-control attribute-key" value="${key}">
             </div>
             <div class="col-auto my-auto">
                 <strong>=</strong>
             </div>
-            <div class="col-4">
+            <div class="col-5">
                 <input type="text" class="form-control attribute-value" value="${value}">
             </div>
             <div class="col-auto ms-auto my-auto">
