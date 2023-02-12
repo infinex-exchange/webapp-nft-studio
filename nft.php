ae35bd01a2fbff65673fa6ea877d9200290a4c04
infinex-exchange <admin@vayamos.cc>
2023-02-12 13:21:44 +0100

    Layout

--- nft.php
+++ nft.php
@@ -105,19 +105,19 @@
         <div class="col-12 pt-4">
             <div class="row">
 	            <div class="col-6 col-lg-auto pe-1">
-		            <button id="nft-submit" type="button" class="btn btn-primary">
+		            <button id="nft-submit" type="button" class="btn btn-primary w-100">
                         <i class="fa-solid fa-floppy-disk"></i>
                         Save changes
                     </button>
 	            </div>
 	            <div class="col-6 col-lg-auto ps-1 pe-1">
-		            <button id="nft-submit-and-mint" type="button" class="btn btn-primary">
+		            <button id="nft-submit-and-mint" type="button" class="btn btn-primary w-100">
                         <i class="fa-solid fa-check"></i>
                         Save and mint
                     </button>
 	            </div>
 	            <div class="col-6 col-lg-auto ps-lg-1 pe-1 pt-2 pt-lg-0 edit-only d-none">
-		            <button id="nft-remove" type="button" class="btn btn-primary">
+		            <button id="nft-remove" type="button" class="btn btn-primary w-100">
                         <i class="fa-solid fa-trash-can"></i>
                         Remove NFT
                     </button>
