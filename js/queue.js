47f7876b821ba1d0cec5588447a5c516c930eef7
infinex-exchange <admin@vayamos.cc>
2023-02-13 23:39:34 +0100

    Queue as test

--- js/queue.js
+++ js/queue.js
@@ -1,4 +1,58 @@
 $(document).ready(function() {
 	window.renderingStagesTarget = 1;
-    $(document).trigger('renderingStage');
 });
+
+$(document).on('authChecked', function() {
+	if(!window.loggedIn) return;
+	
+	window.queueAS = new AjaxScroll(
+        $('#queue-data'),
+        $('#queue-data-preloader'),
+        {
+	        api_key: window.apiKey
+        },
+        function() {
+            this.data.offset = this.offset;
+            var thisAS = this;
+            
+            $.ajax({
+                url: config.apiUrl + '/nft/studio/queue',
+                type: 'POST',
+                data: JSON.stringify(thisAS.data),
+                contentType: "application/json",
+                dataType: "json",
+            })
+            .retry(config.retry)
+            .done(function (data) {
+                if(data.success) {
+                    $.each(data.queue, function(k, v) {
+                        thisAS.append(renderQueueTask(v));
+                    });
+                    
+                    thisAS.done();
+            
+                    if(thisAS.offset == 0)
+                        $(document).trigger('renderingStage');
+                        
+                    if(data.queue.length != 50)
+                        thisAS.noMoreData();
+                } else {
+                    msgBoxRedirect(data.error);
+                    thisAS.done();
+                    thisAS.noMoreData();
+                }
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                msgBoxNoConn(true);
+                thisAS.done();
+                thisAS.noMoreData();
+            });
+        },
+        true,
+        true
+    );
+});
+
+function renderQueueTask(data) {
+	return 'task ' + data.type;
+}
