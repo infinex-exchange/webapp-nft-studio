9405f865ecea63a41b9e589eac5c0668e3c19207
infinex-exchange <admin@vayamos.cc>
2023-02-14 00:41:11 +0100

    Padding

--- queue.php
+++ queue.php
@@ -28,7 +28,7 @@
             </div>
             
             <div class="row">
-                <div id="queue-running" class="col-12 ui-card-light">
+                <div id="queue-running" class="col-12 ui-card-light m-2">
 					<div class="row">
 						<div class="col-12 p-2">
 							<h5>Minting in progress...</h5>
