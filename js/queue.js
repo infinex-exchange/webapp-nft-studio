732f262c72e9d9f01225e6af1cc8afcd26a8ada4
infinex-exchange <admin@vayamos.cc>
2023-02-14 01:07:49 +0100

    Padding

--- js/queue.js
+++ js/queue.js
@@ -61,7 +61,7 @@ $(document).on('authChecked', function() {
 
 function renderQueueTask(data) {
 	return `
-		<div class="col-12">
+		<div class="col-12 py-1">
             <div class="ui-card-light p-2">
 			<div class="row">
 			    <div class="col-2 my-auto text-center">
