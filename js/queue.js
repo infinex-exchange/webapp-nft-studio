583e96c0f033f8d82f1e255ebea584ff6f7cdef3
infinex-exchange <admin@vayamos.cc>
2023-02-14 01:08:47 +0100

    Styling

--- js/queue.js
+++ js/queue.js
@@ -62,7 +62,7 @@ $(document).on('authChecked', function() {
 function renderQueueTask(data) {
 	return `
 		<div class="col-12 py-1">
-            <div class="ui-card-light p-2">
+            <div class="ui-card-light hoverable p-2">
 			<div class="row">
 			    <div class="col-2 my-auto text-center">
 				    <i class="fa-solid ${dictTaskStatusIcon[data.status]} fa-2x"></i>
