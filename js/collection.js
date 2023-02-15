2c26a0aeeceb8924595a337c004ca01dc8cf2866
infinex-exchange <admin@vayamos.cc>
2023-02-15 21:01:10 +0100

    Fix ro

--- js/collection.js
+++ js/collection.js
@@ -180,9 +180,10 @@ $(document).on('authChecked', function() {
                 
                 var ro = (data.status != 'DRAFT');
                 
-                if(ro)
+                if(ro) {
 	                $('#col-name,  #col-description, #col-website, #col-twitter').prop('readonly', true);
 	                $('#select-net, #col-submit, #col-submit-and-mint, #col-remove').prop('disabled', true);
+	            }
                 
                 initUpload('#col-icon', 'nft-col-icon', true, data.icon_url, ro);
 		        initUpload('#col-banner', 'nft-col-banner', true, data.banner_url, ro);
